dd2cb8d29b9ad492f745d132fc20f3d1ff3b7901
Nicholas McGlincy <nmcglincy@gmail.com>
2015-09-30 12:20:18 -0700

    constructing pubmed search term

--- pubmed-search-to-html.R
+++ pubmed-search-to-html.R
@@ -2,6 +2,19 @@
 # NJM, 20150930
 # Make a pubmed search for out group's papers
 library(rentrez)
+library(lubridate)
 
 # Specifically, Nick's papers from a year to the present year
 from.year = 2013
+present.year = year(Sys.Date())
+search.term = paste("Ingolia NT[AUTH] AND ", 
+                    from.year, 
+                    ":", 
+                    present.year, 
+                    "[PDAT]", 
+                    sep = "")
+
+nti.pap = entrez_search(db = "pubmed",
+                        term = search.term,
+                        retmax = 33)
+
