e59ba5331a154369a0255916067db47786345dc5
Nicholas McGlincy <nmcglincy@gmail.com>
2015-10-01 13:35:01 -0700

    clean up ahead of final push

--- pubmed-search-to-html.R
+++ pubmed-search-to-html.R
@@ -4,7 +4,6 @@
 library(rentrez)
 library(lubridate)
 library(stringr)
-
 # Specifically, Nick's papers from a year to the present year
 from.year = 2013
 present.year = year(Sys.Date())
@@ -14,27 +13,18 @@ search.term = paste("Ingolia NT[AUTH] AND ",
                     present.year, 
                     "[PDAT]", 
                     sep = "")
-
 nti.pap = entrez_search(db = "pubmed",
                         term = search.term,
                         use_history = TRUE)
-
 # there's some ambiguity here, I think that because I've invoked use_history I will get the total 
 # list of ids, even if it at some point exceeds 20, but I'm not certain.
-# nti.pap
-# str(nti.pap)
-
 nti.pap.summ = entrez_summary(db = "pubmed",
                               # id = nti.pap$ids,
                               web_history = nti.pap$web_history)
 # Asks me to used either the $ids or the web_history, but not both.
-
-nti.pap.summ
-
-foo = extract_from_esummary(nti.pap.summ, 
-                      c("authors", "title", "fulljournalname", "volume", "pages", "pubdate"), 
-                      simplify = FALSE)
-str(foo)
+foo = extract_from_esummary(nti.pap.summ,
+                            c("authors", "title", "fulljournalname", "volume", "pages", "pubdate"), 
+                            simplify = FALSE)
 header = '<div style="margin: 0 0 0.6em 0; text-indent: -2em; padding-left: 2em;">'
 for (i in 1:length(foo)) {
   print(paste(header,
@@ -45,22 +35,14 @@ for (i in 1:length(foo)) {
               paste(foo[[i]][[3]]),
               "</i> ",
               "<b>",
-              paste(foo[[i]][[4]])
+              paste(foo[[i]][[4]]),
               "</b>: ",
+              paste(foo[[i]][[5]]),
+              " (",
+              str_sub(paste(foo[[i]][[6]]), 1, 4),
+              "). </div>",
               sep = ""))
 }
+# the format of the page numbers are so different, I feel like it's going to be easier to sort it
+# out manually, while I'm sorting the publication types.
 
-
-# this produces a list of lists
-
-
-paste("<div style="margin: 0 0 0.6em 0; text-indent: -2em; padding-left: 2em;">",
-      )
-
-
-<div style="margin: 0 0 0.6em 0; text-indent: -2em; padding-left: 2em;">
-Stern-Ginossar N, Weisburd B, Michalski A, Le VT, Hein
-MY, Huang SX, Ma M, Shen B, Qian SB, Hengel H, Mann M,
-Ingolia NT, Weissman JS. Decoding human
-cytomegalovirus. <i>Science</i> <b>338</b>: 1088 (2012).
-</div>
